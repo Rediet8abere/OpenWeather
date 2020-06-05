8e94d5a301c35d01094c6b728c974824100767af
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-04 18:23:27 -0700

    take input from user

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
   "github.com/gorilla/mux"
 	"encoding/json"
+	"net/url"
 )
 
 type OpenWeather struct {
@@ -51,51 +52,13 @@ type OpenWeather struct {
 	Cod      int    `json:"cod"`
 }
 
+func GetWeather(city string) []byte{
+	safeQuery := url.QueryEscape(city)
 
-
-// func weatherHandler(w http.ResponseWriter, r *http.Request) {
-	// openweather := OpenWeather{}
-	// fmt.Println("openweather: ", openweather)
-	// jsn, err := ioutil.ReadAll(r.Body)
-	// fmt.Println("getting jsn: ", jsn)
-	//
-	// if err != nil {
-	// 	log.Fatal("Error while reading r.Body: ", err)
-	// }
-	// err = json.Unmarshal(jsn, &openweather)
-	// if err != nil {
-	// 	log.Fatal("Decoding error: ", err)
-	// }
-	// log.Printf("Recived: %v\n", openweather)
-
-// }
-func getOpenWeather(body []byte) (*OpenWeather, error) {
-	var s = new(OpenWeather)
-	err := json.Unmarshal(body, &s)
-	if (err != nil) {
-		fmt.Println("error occured during unmarshalling: ", err)
-	}
-	return s, err
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Welcome to my awseome site!</h1>")
-}
-
-func main() {
-	GetRequest()
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", hello)
-	// r.HandleFunc("/weather", weatherHandler)
-	log.Fatal(http.ListenAndServe(":8000", r))
-}
-
-func GetRequest() {
-	url := "https://community-open-weather-map.p.rapidapi.com/weather?q=London"
+	url := fmt.Sprintf("https://community-open-weather-map.p.rapidapi.com/weather?q=%s", safeQuery)
 
 	req, _ := http.NewRequest("GET", url, nil)
+
 
 	req.Header.Add("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
 	req.Header.Add("x-rapidapi-key", "197b9992b3mshf84e47cf0693477p123b73jsnb27522c04ca3")
@@ -107,9 +70,35 @@ func GetRequest() {
 
 	// fmt.Println(res)
 	fmt.Println(string(body))
-	s, err:= getOpenWeather([]byte(body))
-	if (err!=nil) {
-		fmt.Println("error!!")
+	return body
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "<h1>Welcome to my awseome site!</h1>")
+}
+
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func main() {
+	fmt.Print("Enter City: ")   //Print function is used to display output in same line
+  var city string
+  fmt.Scanln(&city)                  // Take input from user
+	
+	openweather := OpenWeather{}
+	respBody := GetWeather(city)
+	err := json.Unmarshal(respBody, &openweather)
+	if (err != nil) {
+		fmt.Println("error occured during unmarshalling: ", err)
 	}
-	fmt.Println(s)
+	fmt.Println("openweather after: ", openweather)
+	// fmt.Fprint(w, openweather)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	// r.HandleFunc("/weather", weatherHandler)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
